bc23b093b967945a70bfbe39a60fe62dfbe122b8
Brian Gerson <brianpgerson@gmail.com>
2015-09-28 21:33:54 -0700

    one more try

--- beepboop.js
+++ beepboop.js
@@ -1,3 +1,11 @@
+// START HEROKU SETUP
+var express = require("express");
+var app = express();
+app.get('/', function(req, res){ res.send('The robot is happily running.'); });
+app.listen(process.env.PORT || 5000);
+// END HEROKU SETUP
+
+
 var Twit = require('twit');
 var Bot = new Twit({
 	consumer_key: "pblDOPyFKEZW44JiqAXhApqRE"
